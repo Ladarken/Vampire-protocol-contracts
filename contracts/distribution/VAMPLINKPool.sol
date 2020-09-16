2365104b9a76e9cb5caf9af6f1436fc7e110b54c
bytefang <stakd@protonmail.com>
2020-09-16 17:24:46 +0200

    Fixed contracts. WIP

--- contracts/distribution/VAMPLINKPool.sol
+++ contracts/distribution/VAMPLINKPool.sol
@@ -1,3 +1,4 @@
+
 /*
 
                                                                                                       
@@ -58,101 +59,10 @@ import "../lib/Address.sol";
 import "../lib/SafeERC20.sol";
 import "../lib/IRewardDistributionRecipient.sol";
 
+pragma solidity ^0.5.17;
 interface VAMP {
     function scalingFactor() external returns (uint256);
 }
-
-/**
- * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
- * the optional functions; to access them see {ERC20Detailed}.
- */
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function mint(address account, uint amount) external;
-    function deposit() external payable;
-    function withdraw(uint) external;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-/
-/**
- * @title SafeERC20
- * @dev Wrappers around ERC20 operations that throw on failure (when the token
- * contract returns false). Tokens that return no value (and instead revert or
- * throw on failure) are also supported, non-reverting calls are assumed to be
- * successful.
- * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
- * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
- */
-library SafeERC20 {
-    using SafeMath for uint256;
-    using Address for address;
-
-    function safeTransfer(IERC20 token, address to, uint256 value) internal {
-        callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
-    }
-
-    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
-        callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
-    }
-
-    function safeApprove(IERC20 token, address spender, uint256 value) internal {
-        // safeApprove should only be called when setting an initial allowance,
-        // or when resetting it to zero. To increase and decrease it, use
-        // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
-        // solhint-disable-next-line max-line-length
-        require((value == 0) || (token.allowance(address(this), spender) == 0),
-            "SafeERC20: approve from non-zero to non-zero allowance"
-        );
-        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
-    }
-
-    function safeIncreaseAllowance(IERC20 token, address spender, uint256 value) internal {
-        uint256 newAllowance = token.allowance(address(this), spender).add(value);
-        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
-    }
-
-    function safeDecreaseAllowance(IERC20 token, address spender, uint256 value) internal {
-        uint256 newAllowance = token.allowance(address(this), spender).sub(value, "SafeERC20: decreased allowance below zero");
-        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
-    }
-
-    /**
-     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
-     * on the return value: the return value is optional (but if data is returned, it must not be false).
-     * @param token The token targeted by the call.
-     * @param data The call data (encoded using abi.encode or one of its variants).
-     */
-    function callOptionalReturn(IERC20 token, bytes memory data) private {
-        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
-        // we're implementing it ourselves.
-
-        // A Solidity high level call has three parts:
-        //  1. The target address is checked to verify it contains contract code
-        //  2. The call itself is made, and success asserted
-        //  3. The return value is decoded, which in turn checks the size of the returned data.
-        // solhint-disable-next-line max-line-length
-        require(address(token).isContract(), "SafeERC20: call to non-contract");
-
-        // solhint-disable-next-line avoid-low-level-calls
-        (bool success, bytes memory returndata) = address(token).call(data);
-        require(success, "SafeERC20: low-level call failed");
-
-        if (returndata.length > 0) { // Return data is optional
-            // solhint-disable-next-line max-line-length
-            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
-        }
-    }
-}
-
 
 contract LPTokenWrapper {
     using SafeMath for uint256;
@@ -188,7 +98,9 @@ contract VAMPLINKPOOL is LPTokenWrapper, IRewardDistributionRecipient {
     IERC20 public vamp = IERC20(0x4De8f3F90b1bFBE535a979B94f0eE94132d7072D);
     uint256 public DURATION = 7 days;
     uint256 public generation = 3;
-    uint256 public initreward = 10000*1e18; //change this
+    uint256 public totalsup = vamp.totalSupply().mul(30).div(100); //change
+    uint256 public genReward = totalsup.mul(11).div(100);
+    uint256 public initreward = genReward.mul(17).div(100);
     uint256 public starttime = 1598707259;
     uint256 public periodFinish = 0;
     uint256 public rewardRate = 0;
@@ -265,61 +177,63 @@ contract VAMPLINKPOOL is LPTokenWrapper, IRewardDistributionRecipient {
         }
     }
 
-      modifier checkhalve(){
-        if (block.timestamp >= periodFinish) {
-            generation = generation.add(1);
-          if (generation == 4){
+     modifier checkhalve() {
+    if (block.timestamp >= periodFinish) {
+        generation = generation.add(1);
+        if (generation == 4) {
             DURATION = 6 days;
-            initreward = initreward.div(66822).mul(10000); 
-            vamp.mint(address(this),initreward);
-             rewardRate = initreward.div(DURATION);
+            genReward = totalsup.mul(16).div(100);
+            initreward = genReward.mul(17).div(100);
+            vamp.mint(address(this), initreward);
+            rewardRate = initreward.div(DURATION);
             periodFinish = block.timestamp.add(DURATION);
             emit RewardAdded(initreward);
-            }else if (generation == 5){
-              DURATION = 5 days;
-              initreward = initreward.div(75).mul(100); 
-            vamp.mint(address(this),initreward);
-             rewardRate = initreward.div(DURATION);
+        } else if (generation == 5) {
+            DURATION = 5 days;
+            genReward = totalsup.mul(21).div(100);
+            initreward = genReward.mul(17).div(100);
+            vamp.mint(address(this), initreward);
+            rewardRate = initreward.div(DURATION);
             periodFinish = block.timestamp.add(DURATION);
             emit RewardAdded(initreward);
-            }
-            else if (generation == 6){
-                DURATION = 3 days;
-                initreward = initreward.div(80).mul(100); 
-            vamp.mint(address(this),initreward);
-             rewardRate = initreward.div(DURATION);
+        } else if (generation == 6) {
+            DURATION = 3 days;
+            genReward = totalsup.mul(27).div(100);
+            initreward = genReward.mul(17).div(100);
+            vamp.mint(address(this), initreward);
+            rewardRate = initreward.div(DURATION);
             periodFinish = block.timestamp.add(DURATION);
             emit RewardAdded(initreward);
-            } else if (generation == 7){
-                uint256 balance = vamp.balanceOf(address(this));
-                require(balance > 0,"Contract is empty, all rewards distributed");
-                vamp.safeTransfer(owner(),balance); //transfer any leftover rewards to the owner to be burned or airdropped.
-            }
-
+        } else if (generation > 6) {
+            uint256 balance = vamp.balanceOf(address(this));
+            require(balance > 0, "Contract is empty, all rewards distributed");
+            vamp.safeTransfer(owner(), balance); //transfer any leftover rewards to the owner to be burned or airdropped.
         }
-        _;
+
     }
+    _;
+}
     
     modifier checkStart(){
         require(block.timestamp > starttime,"not start");
         _;
     }
 
-    function notifyRewardAmount(uint256 reward)
+    function notifyRewardAmount()
         external
         onlyRewardDistribution
         updateReward(address(0))
     {
         if (block.timestamp >= periodFinish) {
-            rewardRate = reward.div(DURATION);
+            rewardRate = initreward.div(DURATION);
         } else {
             uint256 remaining = periodFinish.sub(block.timestamp);
             uint256 leftover = remaining.mul(rewardRate);
-            rewardRate = reward.add(leftover).div(DURATION);
+            rewardRate = initreward.add(leftover).div(DURATION);
         }
-        vamp.mint(address(this),reward);
+        vamp.mint(address(this),initreward);
         lastUpdateTime = block.timestamp;
         periodFinish = block.timestamp.add(DURATION);
-        emit RewardAdded(reward);
+        emit RewardAdded(initreward);
     }
 }
