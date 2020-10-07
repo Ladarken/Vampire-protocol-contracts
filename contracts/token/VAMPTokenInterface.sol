10d80fb2ff667ab5eac713dfd64ef111beef14de
GCU GreyArea <meatfucker@protonmail.com>
2020-10-07 21:18:23 +1100

    Initial refactoring to get truffle test/compile working.
    Need to fix the migrations next to work properly for testing as all previous work was on rolled up contracts via remix.

---
+++ contracts/token/VAMPTokenInterface.sol
@@ -0,0 +1,58 @@
+pragma solidity 0.5.17;
+
+import "./VAMPTokenStorage.sol";
+
+contract VAMPTokenInterface is VAMPTokenStorage {
+
+    event TransactionFailed(address indexed destination, uint index, bytes data);
+
+    event LogRebase(uint256 indexed epoch, uint256 totalSupply);
+
+    /**
+     * @notice EIP20 Transfer event
+     */
+    event Transfer(address indexed from, address indexed to, uint amount);
+
+    /**
+     * @notice EIP20 Approval event
+     */
+    event Approval(address indexed owner, address indexed spender, uint amount);
+
+
+    // FUNCTIONS
+    function rebase(int256 supplyDelta) external returns (uint256);
+
+    function mint(address account, uint256 amount) public returns (bool);
+
+    function burn(uint256 amount) public returns (bool);
+
+    // function _mint(address account, uint256 amount) internal;
+    // function _burn(address account, uint256 value) internal;
+
+    function totalSupply() public view returns (uint256);
+
+    function balanceOf(address who) public view returns (uint256);
+
+    function transfer(address to, uint256 value) public returns (bool);
+
+    function allowance(address owner_, address spender) public view returns (uint256);
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+
+    function approve(address spender, uint256 value) public returns (bool);
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool);
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool);
+
+    function addTransaction(address destination, bytes calldata data) external;
+
+    function removeTransaction(uint index) external;
+
+    function setTransactionEnabled(uint index, bool enabled) external;
+
+    function transactionsSize() external view returns (uint256);
+
+    // function externalCall(address destination, bytes memory data) internal returns (bool);
+
+}
